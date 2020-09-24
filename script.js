02c1b6e9acc169e8b836d5e26cc353af5a4821ab
Richard Nhun <richardnhun@Richards-MacBook-Pro.local>
2020-09-24 10:31:25 -0700

    initial commit

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
+
+
 
 // Show New Quote
 function newQuote() {
@@ -43,6 +46,15 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
